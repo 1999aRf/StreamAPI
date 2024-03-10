7ae0288d8d246161987bb5d7aed6eed6e8e4def0
rinatagliulov <rinat.agliulov@rentready.com>
2024-03-10 14:40:06 -0400

    Домашняя работа по библиотекам сделана и готова к проверке.

--- src/main/java/com/streamAPI/StreamAPI/controller/EmployeeController.java
+++ src/main/java/com/streamAPI/StreamAPI/controller/EmployeeController.java
@@ -1,7 +1,9 @@
 package com.streamAPI.StreamAPI.controller;
 
 import com.streamAPI.StreamAPI.Employee;
+import com.streamAPI.StreamAPI.exception.IllegalArgumentException;
 import com.streamAPI.StreamAPI.service.EmployeeService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,28 +22,41 @@ public class EmployeeController {
 
     @GetMapping("add")
     public Employee add(@RequestParam String firstName, @RequestParam String lastName) {
-        return employeeService.add(firstName, lastName);
+        String[] names = validateAndCapitalizeName(firstName, lastName);
+        return employeeService.add(names[0], names[1]);
     }
-    @GetMapping("add")
+    @GetMapping("add2")
     public Employee add(@RequestParam String firstName,
                         @RequestParam String lastName,
                         @RequestParam int departmentId,
                         @RequestParam int salary) {
-        return employeeService.add(firstName, lastName, departmentId, salary);
+        String[] names = validateAndCapitalizeName(firstName, lastName);
+        return employeeService.add(names[0], names[1], departmentId, salary);
     }
 
     @GetMapping("remove")
     public Employee remove(@RequestParam String firstName, @RequestParam String lastName) {
-        return employeeService.remove(firstName, lastName);
+        String[] names = validateAndCapitalizeName(firstName, lastName);
+        return employeeService.remove(names[0], names[1]);
     }
 
     @GetMapping("find")
     public Employee find(@RequestParam String firstName, @RequestParam String lastName) {
-        return employeeService.find(firstName, lastName);
+        String[] names = validateAndCapitalizeName(firstName, lastName);
+        return employeeService.find(names[0], names[1]);
     }
 
     @GetMapping
     public Collection<Employee> findAll() {
         return employeeService.findAll();
+    }
+
+    private String[] validateAndCapitalizeName(String firstName, String lastName) {
+        if (StringUtils.isAnyEmpty(firstName, lastName) || !firstName.matches("[A-Za-z]+") || !lastName.matches("[A-Za-z]+"))  {
+            throw new IllegalArgumentException();
+        }
+            firstName = StringUtils.capitalize(firstName);
+            lastName = StringUtils.capitalize(lastName);
+            return new String[]{firstName, lastName};
     }
 }
